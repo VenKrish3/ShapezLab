02b88b83ba5e345a18c3bc30d7fcb0f4855ce1e4
VenKrish3 <kv72810@mail.harpercollege.edu>
2024-09-24 15:10:57 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <cassert>
+#include "Shape.hpp"
+#include "Circle.hpp"
+#include "Rectangle.hpp"
+#include "RightTriangle.hpp"
 
 using namespace std;
 
 int main()
 {
-	
+	Circle circle = new Circle();
+	Rectangle rect = new rectangle();
+	RightTriangle trang = new RightTriangle();
+	assert(circle.getArea(3) == 28.27);
+	assert(rect.getArea(3,3) == );
+	assert(trang.getArea(3,3) == );
+	assert(circle.getPerimeter(3,3) == );
+	assert(rect.getPerimeter(3,3) == );
+	assert(trang.getPerimeter(3,3) == );
 }
